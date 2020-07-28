b818076f7c2a4f29d09fea9ad73c54d232b66592
Leonid0208 <themusik.batbka@gmail.com>
2020-07-29 02:57:28 +0300

    Initialize Django project

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', include('social_django.urls')),
+    # path('', include('social_django.urls')),
     path('', main),
     path('logout/', logout_v, name='logout_url')
 ]
