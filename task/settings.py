4fa0fdc5e442bc9cc9a400e8a7c621ea13a8f1a0
Leonid0208 <themusik.batbka@gmail.com>
2020-07-29 03:39:18 +0300

    Initialize Django project

--- task/settings.py
+++ task/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,8 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# PROJECT_ROOT = BASE_DIR
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_ROOT = BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
 
@@ -142,4 +142,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
