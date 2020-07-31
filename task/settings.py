f6fa267df5c5e8ed9d2722fa277342ad9bf59398
Leonid0208 <themusik.batbka@gmail.com>
2020-07-31 13:18:03 +0300

    Update

--- task/settings.py
+++ task/settings.py
@@ -138,5 +138,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # django_heroku.settings(locals())
